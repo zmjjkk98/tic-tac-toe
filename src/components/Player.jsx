5a59ac65136a1b45185202cbd93065d200ff2653
zmjjkk98 <ysyt3199@gmail.com>
2025-01-31 02:47:31 +0900

    FEAT: 플레이어 이름변경제어 추가
    useState를 활용하여 Edit버튼 클릭시 playerName을
    input type으로 변경 + 버튼 Save로 변경 시킴
    handleEditClick 함수에서 현재 상태값 false->true 혹은 true->false로 변경시
    (이전 상태값 기준 변경시) 함수로 전달
    리액트 상태 변화의 스케줄을 조율하기 때문에 !isEditing 같이 전달할 경우
    즉시 반영되지 않기때문

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  // let btnCaption = "Edit";
+
+  if (isEditing) {
+    playerName = <input type="text" required value={name} />;
+    // btnCaption = "Save"
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
