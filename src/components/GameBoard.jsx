956e84ee04d2eb8fa8a58f7f00a07b5aa18292c3
zmjjkk98 <ysyt3199@gmail.com>
2025-02-02 00:50:24 +0900

    FEAT: 버튼 중복 클릭 방지
    button disabled 속성을 사용

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -35,7 +35,10 @@ export default function GameBoard({ onSelectSquare, turns }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
                   {playerSymbol}
                 </button>
               </li>
